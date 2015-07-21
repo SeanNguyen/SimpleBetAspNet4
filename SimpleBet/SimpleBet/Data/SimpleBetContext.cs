388a8c6b50832115a382f33b4ae69405c33bf376
Sean Nguyen <firestormkjc.1993@gmail.com>
2015-07-21 13:03:19 +0800

    added publish profile

--- SimpleBet/SimpleBet/Data/SimpleBetContext.cs
+++ SimpleBet/SimpleBet/Data/SimpleBetContext.cs
@@ -8,17 +8,17 @@ namespace SimpleBet.Data
 
         //use different connection string for different build setting
 
-        private static string connectionString = @"Data Source=(localdb)\mssqllocaldb;
-                                                Initial Catalog=SimpleBet;
-                                                Trusted_Connection=False;
-                                                Connection Timeout=30;
-                                                MultipleActiveResultSets=true;";
+//        private static string connectionString = @"Data Source=(localdb)\mssqllocaldb;
+//                                                Initial Catalog=SimpleBet;
+//                                                Trusted_Connection=False;
+//                                                Connection Timeout=30;
+//                                                MultipleActiveResultSets=true;";
 
         //private static string connectionString = @"Server=tcp:pvuq1gyimf.database.windows.net,1433;Database=simplebet;
         //                                        User ID = simplebet@pvuq1gyimf;Password=Simple123; Trusted_Connection=False;
         //                                        Encrypt=True;Connection Timeout = 30;";
 
-        //private static string connectionString = @"Data Source=SnnappVM;Initial Catalog=Test_Arcadier_Com20150318;Persist Security Info=True;User ID=dbuser;Password=Passw0rd";
+        private static string connectionString = @"Data Source=SnnappVM;Initial Catalog=Test_Arcadier_Com20150318;Persist Security Info=True;User ID=dbuser;Password=Passw0rd";
 
 
         public SimpleBetContext() : base(connectionString)
